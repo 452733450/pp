0f3d402590bc3a150e7bbea5004ea468c2509691
ypp <452733450@qq.com>
2020-05-27 15:22:34 +0800

    test

--- src/main/java/com/pp/pp/PpApplication.java
+++ src/main/java/com/pp/pp/PpApplication.java
@@ -1,10 +1,13 @@
 package com.pp.pp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PpApplication {
+    private static final Logger logger = LoggerFactory.getLogger(PpApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(PpApplication.class, args);
